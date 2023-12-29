800378ee66d9c507e791ad88596815422ca24b55
kritan <kritanshrestha560@gmail.com>
2023-12-29 22:21:54 +0545

    dark mode added select options

--- src/app/(components)/modal/2mapModal/MapModal.tsx
+++ src/app/(components)/modal/2mapModal/MapModal.tsx
@@ -1,11 +1,12 @@
 import useCountries from "@/app/hooks/useCountries";
 import { LatLngExpression } from "leaflet";
 import dynamic from "next/dynamic";
-import React, { useId, useMemo, useState } from "react";
+import React, { useEffect, useId, useMemo, useState } from "react";
 import { FieldValues, UseFormSetValue, UseFormWatch } from "react-hook-form";
 import ReactSelect, { SingleValue } from "react-select";
 import Body from "../../body/Body";
 import Heading from "../../heading/Heading";
+import { useTheme } from "next-themes";
 
 export type CountryType = ReturnType<typeof useCountries> extends (infer U)[]
     ? U
@@ -36,6 +37,8 @@ const MapModal: React.FC<MapModalProps> = ({
     );
 
     const conuntryValue: CountryType = watch(id);
+    const { theme, systemTheme } = useTheme();
+    const [invert, setInvert] = useState<boolean>();
 
     // handle country change
     function handleCountryChange(e: SingleValue<CountryType>) {
@@ -43,21 +46,41 @@ const MapModal: React.FC<MapModalProps> = ({
         setValue(id, e);
     }
 
+    useEffect(() => {
+        if (theme == "dark") {
+            setInvert(true);
+        } else if (theme == "light") {
+            setInvert(false);
+        } else if (theme == "system") {
+            if (systemTheme == "dark") {
+                setInvert(true);
+            } else if (systemTheme == "light") {
+                setInvert(false);
+            }
+        }
+    }, [theme]);
+
     return (
         <div>
             <Heading subtitle={subtitle} title={title} />
             <Body className="">
-                <div className="flex flex-col gap-y-3">
+                <div className="flex flex-col gap-y-3 ">
                     <ReactSelect
                         instanceId={useId()} // you need this instanceId without it give react-select-4-live-region warning error
-                        className="z-[9999] dark:text-blue-900"
+                        className={`z-[9999] dark:text-blue-400 ${
+                            invert ? "invert" : "invert-0"
+                        }`}
                         isClearable
                         placeholder="AnyWhere"
                         options={useCountries()}
                         value={conuntryValue}
                         onChange={(e) => handleCountryChange(e)}
                         formatOptionLabel={(fieldValue) => (
-                            <div className="flex gap-x-2">
+                            <div
+                                className={`flex gap-x-2 ${
+                                    invert ? "invert" : "invert-0"
+                                }`}
+                            >
                                 <span>{fieldValue.flag}</span>
                                 <span>{fieldValue.value}</span>
                             </div>
